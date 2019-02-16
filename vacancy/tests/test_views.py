273ec8af8d267773c69ee82c6c9d2a986b731806
Nekr0bz <Nekr0bz@yandex.ru>
2019-02-16 15:33:55 +0300

    Add and test: List View

--- vacancy/tests/test_views.py
+++ vacancy/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-
 from django.urls import reverse
+
+from vacancy.models import VacancyModel
+from .factories import VacancyFactory
 
 
 class IndexViewTest(TestCase):
@@ -17,6 +19,33 @@ class IndexViewTest(TestCase):
         self.assertTemplateUsed(resp, 'index.html')
 
 
+class ListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        VacancyFactory.create_batch(5)
+
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get('/vacancy/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse('vacancy:vacancy_list'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('vacancy:vacancy_list'))
+        self.assertTemplateUsed(resp, 'list.html')
+
+    def test_view_have_correct_len_list_objects(self):
+        resp = self.client.get(reverse('vacancy:vacancy_list'))
+        object_list = resp.context['object_list']
+        self.assertTrue(len(object_list) == 5)
+
+    def test_view_have_correct_type_list_objects(self):
+        resp = self.client.get(reverse('vacancy:vacancy_list'))
+        object_list = resp.context['object_list']
+        for obj in object_list:
+            self.assertTrue(isinstance(obj, VacancyModel))
 
 
 
--- vacancy/views.py
+++ vacancy/views.py
@@ -2,13 +2,16 @@ from django.views.generic import DetailView
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
+from .models import VacancyModel
+
 
 class IndexPageView(TemplateView):
     template_name = "index.html"
 
 
 class VacancyListView(ListView):
-    pass
+    template_name = "list.html"
+    model = VacancyModel
 
 
 class VacancyDetailView(DetailView):
